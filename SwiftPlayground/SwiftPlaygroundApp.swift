abb54a9a875be7946fd90d77bbde12e01fa684e1
Peter Chege <peterkagure@gmail.com>
2026-02-14 12:46:45 +0300

    Initial Commit

---
+++ SwiftPlayground/SwiftPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPlaygroundApp.swift
+//  SwiftPlayground
+//
+//  Created by Peter Chege on 14/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
